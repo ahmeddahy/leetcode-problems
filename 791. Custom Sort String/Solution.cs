af74926b313f68363f961e4c394b56091d668767
ahmeddahy <ahmeddahy380@gmail.com>
2024-09-28 14:08:17 +0300

    add problem

---
+++ 791. Custom Sort String/Solution.cs
@@ -0,0 +1,34 @@
+using System.Text;
+
+public class Solution
+{
+    public string CustomSortString(string order, string s)
+    {
+        var charsCount = new int[26];
+        var res = new StringBuilder();
+
+        foreach (char c in s)
+            charsCount[c - 'a']++;
+
+        foreach (char c in order)
+        {
+            while (charsCount[c - 'a'] > 0)
+            {
+                res.Append(c);
+                charsCount[c - 'a']--;
+            }
+        }
+
+        foreach (char c in s)
+        {
+            while (charsCount[c - 'a'] > 0)
+            {
+                res.Append(c);
+                charsCount[c - 'a']--;
+            }
+        }
+
+
+        return res.ToString();
+    }
+}
